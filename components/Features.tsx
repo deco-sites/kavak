cd11d8c4a30ea03bf3cdf7b0d0423ae760397a2c
loliveirawebdev <loliveirawebdev@gmail.com>
2023-04-13 18:44:53 -0300

    feat: add features section

--- components/Features.tsx
+++ components/Features.tsx
@@ -3,12 +3,10 @@ import type { Image as LiveImage } from "deco-sites/std/components/types.ts";
 
 export interface Props {
   title: string;
-  store: Array<{
+  cards: Array<{
     image: LiveImage;
-    name: string;
-    address: string;
-    action: string;
-    href: string;
+    title: string;
+    description: string;
   }>;
   action: {
     label: string;
@@ -16,34 +14,28 @@ export interface Props {
   };
 }
 
-export default function StoreCards(props: Props) {
-  const { title, store, action } = props;
+export default function Features(props: Props) {
+  const { title, cards, action } = props;
 
   return (
     <div class="flex flex-col items-center justify-center bg-primary-light text-primary-dark p-8 gap-12">
       <h1 class="text-3xl font-title">{title}</h1>
 
       <ul class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 md:gap-10 lg:gap-12">
-        {store.map((store) => (
-          <li class="w-full md:max-w-[300px] overflow-hidden rounded-lg shadow-lg">
+        {cards.map((card) => (
+          <li class="w-full md:max-w-[350px] overflow-hidden rounded-lg shadow-lg">
             <div class="flex flex-col gap-2 h-full">
               <img
                 width={300}
-                height={168}
-                alt={store.name}
-                src={store.image}
-                class="object-cover w-full md:w-[300px] h-[168px] block"
+                height={235}
+                alt={card.title}
+                src={card.image}
+                class="object-cover w-full md:w-[350px] h-[235px] block"
               />
 
               <div class="p-4 flex flex-col gap-4 h-full">
-                <span class="text-xl font-semibold">{store.name}</span>
-                <span class="text-gray-500">{store.address}</span>
-
-                <div class="flex justify-end md:flex-col md:flex-1 mt-auto">
-                  <Button href={store.href} type="tertiary">
-                    {store.action}
-                  </Button>
-                </div>
+                <span class="text-2xl font-semibold">{card.title}</span>
+                <span class="text-lg mt-auto">{card.description}</span>
               </div>
             </div>
           </li>
