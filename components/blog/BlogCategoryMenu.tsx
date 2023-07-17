a8741a67dbfa0bdfc51c67fa6829eabe40869b94
Rafael-Oliveira7 <rafasoli10@gmail.com>
2023-07-17 20:22:09 -0300

    Revert "Adicionando butter ao projeto"
    This reverts commit 7ee468aea345dbf84031ae36395dfdc16590c57f.

--- components/blog/BlogCategoryMenu.tsx
+++ components/blog/BlogCategoryMenu.tsx
@@ -1,7 +1,10 @@
 import type { LoaderReturnType } from "$live/types.ts";
 import { Container } from "deco-sites/kavak/components/ui/Container.tsx";
 import { SectionTitle } from "deco-sites/kavak/components/ui/SectionTitle.tsx";
-import type { BlogPage, Category } from "../../std/commerce/butterCMS/types.ts";
+import type {
+  BlogPage,
+  Category,
+} from "../../std/commerce/butterCMS/types.ts";
 import { BlogBreadcrumb } from "./BlogBreadcrumb.tsx";
 
 export interface Props {
